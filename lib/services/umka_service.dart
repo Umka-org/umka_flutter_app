ed80960a09cd7f573f1372d754324f2eb8216492
a <ruan65@gmail.com>
2021-11-11 22:00:29 +0300

    run on the Web

--- lib/services/umka_service.dart
+++ lib/services/umka_service.dart
@@ -1,17 +1,17 @@
+import 'package:build_grpc_channel/build_grpc_channel.dart';
+import 'package:flutter/foundation.dart';
 import 'package:grpc/grpc.dart';
 import 'package:umka_proto/generated/umka.pbgrpc.dart';
 
-ClientChannel _createChannel(String host) => ClientChannel(
-      host,
-      port: 5555,
-      options: ChannelOptions(credentials: ChannelCredentials.insecure()),
-    );
+const host = 'http://127.0.0.1';
+
+int get port => kIsWeb ? 8888 : 5555;
 
 class UmkaService {
   late final UmkaClient stub;
 
   UmkaService() {
-    stub = UmkaClient(_createChannel('127.0.0.1'));
+    stub = UmkaClient(buildGrpcChannel(host: host, port: port, secure: false));
   }
 
   Future<Question> getRandomQuestion(Student student) async {
